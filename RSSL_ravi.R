058ae13b90951941a9fd4828698d8237279c506a
Ravi Kiran Chikkala <97979636+formallinguist@users.noreply.github.com>
2023-03-01 00:33:36 +0100

    Add files via upload

---
+++ RSSL_ravi.R
@@ -0,0 +1,38 @@
+
+getwd()
+
+set.seed(1)
+
+df <- generate2ClassGaussian(2000,d=2,var=0.6,expected=TRUE)
+
+classifiers <- list("LS"=function(X,y,X_u,y_u) {
+  LeastSquaresClassifier(X,y,lambda=0)
+},
+"Self"=function(X,y,X_u,y_u){
+  SelfLearning(X,y,X_u,LeastSquaresClassifier)})
+
+measures <- list("Accuracy" =  measure_accuracy)
+
+lc1 <- LearningCurveSSL(as.matrix(df[,1:2]),
+                        df$Class,classifiers=classifiers,measures=measures,
+                        type="fraction",test_fraction = 0.5,repeats=3)
+
+plot(lc1)
+
+
+iris <- read.csv("iris.csv", header = TRUE, sep = ",")
+
+class(iris)
+
+View(iris)
+
+class(iris$Species)
+
+iris$variety <- as.factor(iris$Species)
+
+lc2 = LearningCurveSSL(as.matrix(iris[1:4]), iris$variety,
+                       classifiers = classifiers, measures = measures,
+                       type = "fraction", fracs = seq(0.1,0.8,0.1),
+                       test_fraction = 0.5, repeats = 3)
+
+plot(lc2)
